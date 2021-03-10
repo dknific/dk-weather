c4946cddde5eea2c75e02c9c2470811d67d3a4d0
Davey Knific <dknific@gmail.com>
2021-03-09 17:02:39 -0800

    mobile and desktop split up

--- src/components/ForecastHeroCard.js
+++ src/components/ForecastHeroCard.js
@@ -5,6 +5,7 @@ import '../styles/ForecastHeroCard.css';
 
 function ForecastHeroCard(props) {
   const { forecast, date } = props;
+  console.log(forecast);
 
   return (
     <div className="forecast-hero-card">
@@ -13,18 +14,33 @@ function ForecastHeroCard(props) {
         <p>{date}</p>
       </div>
       <div className="weather">
-        <div className="hero-info">
-          <p>Feels like: {forecast.feels_like.day}&#176; F</p>
-          <p className="humidity">Humidity: {forecast.humidity}</p>
+
+        <div className="desktop-container">
+          <div className="hero-info">
+            <h1>{parseInt(forecast.temp.day)}&#176; F</h1>
+            <p>Feels like: {forecast.feels_like.day}&#176; F</p>
+          </div>
+          <div className="weather-glyph">
+            <WeatherIcon weatherType={forecast.weather[0].main} />
+            <p>{forecast.weather[0].main}</p>
+          </div>
+          <div className="hero-high-low">
+            <p>High: {forecast.temp.max}&#176; F</p>
+            <p>Low: {forecast.temp.min}&#176; F</p>
+          </div>
         </div>
-        <div className="weather-glyph">
-          <WeatherIcon weatherType={forecast.weather[0].main} />
-          <p>{forecast.weather[0].main}</p>
-        </div>
-        <div className="hero-divider" />
-        <div className="hero-high-low">
-          <p>High: {forecast.temp.max}&#176; F</p>
-          <p>Low: {forecast.temp.min}&#176; F</p>
+
+        <div className="mobile-container">
+          <div className="weather-glyph">
+            <WeatherIcon weatherType={forecast.weather[0].main} />
+            <p>{forecast.weather[0].main}</p>
+          </div>
+          <div className="hero-divider" />
+          <div className="mobile-info">
+            <h1>{parseInt(forecast.temp.day)}&#176; F</h1>
+            <p>High: {parseInt(forecast.temp.max)}&#176; F</p>
+            <p>Low: {parseInt(forecast.temp.min)}&#176; F</p>
+          </div>
         </div>
       </div>
     </div>
